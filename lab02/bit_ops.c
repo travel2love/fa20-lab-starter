cc9d84607f160edc5fcf82286b1e1996957751c8
travel2love <irelia2@qq.com>
2024-05-20 16:53:39 +0800

    with

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -5,6 +5,9 @@
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
                  unsigned n) {
+    x = x << (31 - n);
+    x = x >> 31;
+    return x;
     // YOUR CODE HERE
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
@@ -17,11 +20,32 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    unsigned temp = *x;
+    if(v == 1) {
+      unsigned u = (1 << n);
+      temp = temp | u;
+      *x = temp;
+    }
+    else if(v == 0){
+	temp = ~temp;    
+	unsigned k = (1 << n);
+	temp = temp | k;
+	temp = ~temp;
+	*x = temp;
+    }
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    unsigned temp = *x;
+    unsigned a = get_bit(temp, n);
+    if(a == 1){
+      set_bit(x, n, 0);
+    }
+    else{
+      set_bit(x, n, 1);
+    }
 }
 
